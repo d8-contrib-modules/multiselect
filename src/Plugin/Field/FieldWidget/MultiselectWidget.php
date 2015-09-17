5be09a3acb86dd323d1adb06ef103e86f8cbb1a9
Adam Bergstein <adam.bergstein@acquia.com>
2015-09-17 11:57:06 -0400

    Adjusting form widget

--- src/Plugin/Field/FieldWidget/MultiselectWidget.php
+++ src/Plugin/Field/FieldWidget/MultiselectWidget.php
@@ -7,9 +7,10 @@
 
 namespace Drupal\multiselect\Plugin\Field\FieldWidget;
 
-use Drupal\options\Plugin\Field\FieldWidget\OptionsWidgetBase;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsWidgetBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Plugin implementation of the 'multiselect' widget.
@@ -34,11 +35,11 @@ class MultiselectWidget extends OptionsWidgetBase {
   /**
    * {@inheritdoc}
    */
-  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, array &$form_state) {
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     $element = parent::formElement($items, $delta, $element, $form, $form_state);
     // Prepare some properties for the child methods to build the actual form element.
     $this->required = $element['#required'];
-    $this->multiple = $this->fieldDefinition->getFieldStorageDefinition()->isMultiple();
+    $multiple = $this->fieldDefinition->getFieldStorageDefinition()->isMultiple();
     $this->has_value = isset($items[0]->{$this->column});
 
     $options = $this->getOptions($items[$delta]);
@@ -48,7 +49,7 @@ class MultiselectWidget extends OptionsWidgetBase {
       '#type' => 'multiselect',
       '#size' => $this->getSetting('size'),
       '#options' => $options,
-      '#multiple' => $this->multiple,
+      '#multiple' => $multiple,
       '#key_column' => $this->column,
       '#default_value' => $selected,
     );
@@ -63,34 +64,31 @@ class MultiselectWidget extends OptionsWidgetBase {
    * @param array $form_state
    *   The form state.
    */
-  public static function validateElement(array $element, array &$form_state) {
-    if ($element['#required'] && $element['#value'] == '_none') {
-      \Drupal::formBuilder()->setError($element, $form_state, t('!name field is required.', array('!name' => $element['#title'])));
-    }
-
+  public static function validateElement(array $element, FormStateInterface &$form_state) {
+    parent::validateElement($element, $form_state);
     // Massage submitted form values.
     // Drupal\Core\Field\WidgetBase::submit() expects values as
     // an array of values keyed by delta first, then by column, while our
     // widgets return the opposite.
 
-    if (is_array($element['#value'])) {
-      $values = array_values($element['#value']);
+    if (is_array($element ['#value'])) {
+      $values = array_values($element ['#value']);
     }
     else {
-      $values = array($element['#value']);
+      $values = array($element ['#value']);
     }
 
     // Filter out the 'none' option. Use a strict comparison, because
     // 0 == 'any string'.
     $index = array_search('_none', $values, TRUE);
     if ($index !== FALSE) {
-      unset($values[$index]);
+      unset($values [$index]);
     }
 
     // Transpose selections from field => delta to delta => field.
     $items = array();
     foreach ($values as $value) {
-      $items[] = array($element['#key_column'] => $value);
+      $items [] = array($element ['#key_column'] => $value);
     }
     NestedArray::setValue($form_state['values'], $element['#parents'], $items);
   }
